ae820e03a7c7d3247a5736544190980f9b4fb1a0
JamesRob123 <JamesRob5873@gmail.com>
2021-12-06 18:55:53 +0100

    day6 solutions

---
+++ day6/solution.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <numeric>
+
+std::vector<int> get_input(const std::string& file_name) {
+    std::ifstream fs {file_name};
+    if (!fs.is_open()) {
+        throw std::runtime_error {"Can't open file"};
+    }
+
+    std::vector<int> input {};
+    int n;
+    while (fs >> n) {
+        input.push_back(n);
+        fs.get();
+    }
+
+    return input;
+}
+
+int solve1(std::vector<int> input) {
+    for (int i = 0; i < 80; ++i) {
+        auto size = input.size();
+        // Update day
+        for (int i = 0; i < size; ++i) {
+            //std::cout << vec[i] << ',';
+            if (input[i] == 0) {
+                input[i] = 6;
+                input.push_back(8);
+            } else {
+                --input[i];
+            }
+        }
+    }
+    return input.size();
+}
+
+long long solve2(std::vector<int>& input) {
+    std::vector<long long> fishes (9);
+
+    // Every fish can only have a value 0 - 8 so we can compress them
+    for (int n : input) {
+        ++fishes[n];
+    }
+
+    for (int i = 0; i < 256; ++i) {
+        // Simulating day i
+        std::rotate(fishes.begin(), fishes.begin() + 1, fishes.begin() + 7);
+        long long temp = fishes[6];
+        fishes[6] += fishes[7];
+        fishes[7] = fishes[8];
+        fishes[8] = temp;
+    }
+
+    return std::accumulate(fishes.begin(), fishes.end(), 0LL);
+}
+
+int main() {
+    auto input = get_input("input.txt");
+    std::cout << solve1(input) << '\n';
+    std::cout << solve2(input) << '\n';
+
+    return 0;
+}
